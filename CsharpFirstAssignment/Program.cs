78000229ceeb274ce7ab1e1a188bc704da3d3871
Hammad2625 <68948000+Hammad2625@users.noreply.github.com>
2023-03-17 17:50:47 +0500

    Add project files.

---
+++ CsharpFirstAssignment/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpFirstAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Third Task */
+
+            /*Console.WriteLine("Please enter the value of A");
+            int Value = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the value of B");
+            int Value1 = Convert.ToInt32(Console.ReadLine());
+            if (Value == Value1)
+            {
+
+                Console.WriteLine("Both intergers are Equal!");
+            }
+            else
+            {
+
+                Console.WriteLine("Both intergers are not Equal!");
+            }
+            */
+            /* Second Task */
+
+            /*Console.WriteLine("Please enter the value of a");
+            int Value = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the value of b");
+            int Value1 = Convert.ToInt32(Console.ReadLine());
+
+            if ((Value < 0 && Value1 > 0) || (Value > 0 && Value1 < 0))
+            {
+
+                Console.WriteLine("True!");
+            }
+            else
+            {
+                Console.WriteLine("False");
+
+            }*/
+
+            /* First Task */
+
+            int a = 10;
+            int b = 20;
+            Console.WriteLine("This is the value of a before Swapping =" + a);
+            Console.WriteLine("This is the value of b before Swapping =" + b);
+
+            Console.WriteLine("This is the value of a after Swapping =" + b);
+            Console.WriteLine("This is the value of b after Swapping =" + a);
+
+
+
+
+        }
+    }
+}
